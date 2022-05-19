5401e5ab8432e91f36ba221ae0aa2d596bad0986
lioshounet <1226741725@qq.com>
2022-05-19 12:07:52 +0800

    基本布局

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="app">
-    <pageHeader></pageHeader>
-
-    <funBar></funBar>
-    <aSide></aSide>
-    <pageMain></pageMain>
+    <pageHeader id="pageHeader"></pageHeader>
+    <funBar id="funBar"></funBar>
+    <div class="funBox">
+      <aSide id="aSide"></aSide> <pageMain id="pageMain"></pageMain>
+    </div>
   </div>
 </template>
 
-<style>
+<style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -29,7 +29,43 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+#pageHeader {
+  width: 100%;
+  height: 300px;
+  background-color: rgb(255, 0, 0);
+}
+#funBar {
+  width: 60%;
+  position: relative;
+  margin-left: 20%;
+  height: 75px;
+  background-color: rgb(72, 255, 0);
+}
+
+.funBox {
+  width: 60%;
+  position: relative;
+  margin-left: 20%;
+  // background-color: rgb(208, 255, 0);
+  #aSide {
+    width: 20%;
+    position: absolute;
+    height: 200px;
+    background-color: rgb(0, 255, 234);
+    margin-right: 0;
+  }
+  #pageMain {
+    width: 80%;
+    left: 20%;
+    position: absolute;
+    height: 200px;
+    background-color: rgb(0, 4, 255);
+    margin-right: 0;
+  }
+}
 </style>
+
 
 
 <script>
